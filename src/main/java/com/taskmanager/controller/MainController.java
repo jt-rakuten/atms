781930735bbe9922304adade9340c67400bedecd
Tran, Jonathan | INPD <ts-jonathan.tran@rakuten.com>
2025-01-20 17:33:39 +0900

    test push

--- src/main/java/com/taskmanager/controller/MainController.java
+++ src/main/java/com/taskmanager/controller/MainController.java
@@ -17,6 +17,7 @@ import javafx.scene.control.ListView;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
+//test push
 public class MainController {
     @FXML
     private ListView<Task> taskListView;
